48c797113466120eee1effee659782df170b740d
MUHAMMAD ASHRAF B. KAMARULZAMAN KPM-Guru <ashrafkrai@gmail.com>
2026-09-14 12:19:18 +0800

    icon

--- sw.js
+++ sw.js
@@ -3,7 +3,11 @@ const ASSETS=['/','/index.html','/styles.css','/app.js','/manifest.json',
   '/favicon.png','/apple-touch-icon.png',
   '/icons/icon-192.png','/icons/icon-512.png',
   '/icons/icon-maskable-192.png','/icons/icon-maskable-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(async c=>{
+  await Promise.all(ASSETS.map(async asset=>{
+    try { await c.add(asset); } catch (error) { console.warn('Aset cache gagal:', asset, error); }
+  }));
+})));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET') return;
